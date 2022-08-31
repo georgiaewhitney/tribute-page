3a87eae9e1045eb48f5a20a950ae7e25417193bc
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-08-31 13:02:14 +0100

    added date and styling improvements

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ function displayTime() {
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
 
-  let currentTime = `${hour}:${minutes}:${seconds}`
+  hour < 10 ? "0" + hour : hour;
+  minutes < 10 ? "0" + minutes : minutes;
+  seconds < 10 ? "0" + seconds : seconds;
+
+
+  let currentTime = `${hour}:${minutes}:${seconds}`;
 
   document.getElementById("clock").innerHTML = currentTime;
 }
 displayTime();
 setInterval(displayTime, 1000);
+
+function displayDate() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  day < 10 ? "0" + day : day;
+  month < 10 ? "0" + month : month;
+  
+  let currentDate = `${day}/${month}/${year}`;
+
+  document.getElementById("date").innerHTML = currentDate;
+}
+
+displayDate();
+setInterval(displayDate, 1000);
